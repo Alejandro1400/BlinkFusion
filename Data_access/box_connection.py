362ce06fe3f021d1f1583afd5266d02947d3856f
Alejandro1400 <a.salgadom@uniandes.edu.co>
2024-09-23 12:57:49 -0500

    Feat: Added basic functions for BoxSDK connection

--- Data_access/box_connection.py
+++ Data_access/box_connection.py
@@ -1,4 +1,6 @@
 from boxsdk import OAuth2, Client
+import tkinter as tk
+from tkinter import filedialog
 
 TOKEN = 'RKGvqKckeOSo3sLdNb66vHJsHx59GHMJ'
 
@@ -31,10 +33,36 @@ def print_folder_contents(folder, prefix=""):
             # Recursively print the contents of the sub-folder
             print_folder_contents(box.folder(item.id), prefix + "  ")
 
+# Function to download a .tif file
+def download_first_tif_file(folder):
+    items = folder.get_items()
+    for item in items:
+        if item.type == 'file' and item.name.endswith('.tif'):
+            downloaded_file = item.get().content()
+            with open(item.name, 'wb') as open_file:
+                open_file.write(downloaded_file)
+            print(f"Downloaded {item.name}")
+            break
+
+# Function to upload a file
+def upload_file_to_folder(folder_id):
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_name = file_path.split('/')[-1]
+        with open(file_path, 'rb') as file_stream:
+            box.folder(folder_id).upload_stream(file_stream, file_name)
+        print(f"Uploaded {file_name} to Folder ID {folder_id}")
+    else:
+        print("No file selected")
+
 # Find and print the contents of the "For Alejandro" folder hierarchically
 alejandro_folder = find_folder_by_name("For Alejandro", MY_FOLDER_ID)
 if alejandro_folder:
     print('For Alejandro folder found:')
     print_folder_contents(alejandro_folder, "  ")
+    download_first_tif_file(alejandro_folder)
+    upload_file_to_folder(alejandro_folder.id)
 else:
     print("Folder 'For Alejandro' not found.")
